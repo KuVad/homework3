65bf65bf11d728012e697eb0672594a2e6cb9879
kuvad <brosockobry@gmail.com>
2023-06-29 21:00:51 +0500

    add

---
+++ Task21/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите координаты точки A (x, y, z):");
+int xA = int.Parse(Console.ReadLine());
+int yA = int.Parse(Console.ReadLine());
+int zA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B (x, y, z):");
+int xB = int.Parse(Console.ReadLine());
+int yB = int.Parse(Console.ReadLine());
+int zB = int.Parse(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow((xB - xA),2) + Math.Pow((yB - yA),2) + Math.Pow((zB - zA),2));
+
+Console.WriteLine($"Длина между А и В равна {result}");
---
+++ Task23/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+int i = 1;
+
+while (i <= n)
+{
+    double result = Math.Pow(i, 3);
+    Console.Write(result + " ");
+    i++;
+}
